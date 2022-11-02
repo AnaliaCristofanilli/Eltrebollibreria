10605c825b85444eb48df3fd2bf32723ec3a0aec
Ramiro <ramifazio@gmail.com>
2022-11-02 09:51:40 -0300

    Sugerencias!

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import { FaShoppingCart } from 'react-icons/fa';
+import React from "react";
+import { FaShoppingCart } from "react-icons/fa";
 
 export default function CartWidget() {
   return (
-    <div >
-       <FaShoppingCart size={32}/>
+    <div>
+      <FaShoppingCart size={32} />
     </div>
-  )
+  );
 }
